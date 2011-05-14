c2395fc5df9c25ba060b7e339de3cfed737944e8
borislav <b.manolov@gmail.com>
2011-05-14 15:20:16 +0200

    [js] open edit window through click+metakey

--- src/Chitanka/LibBundle/Resources/public/js/main.js
+++ src/Chitanka/LibBundle/Resources/public/js/main.js
@@ -107,7 +107,7 @@ function enhanceModifying()
 			return false;
 		})
 		.delegate("a[data-edit]", "click", function(event){
-			if (event.altKey) {
+			if (event.metaKey) {
 				window.open($(this).data("edit"));
 				return false;
 			}
