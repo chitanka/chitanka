eddb0696f9fa5849e3235ef81fa7fd6872fd195a
borislav <b.manolov@gmail.com>
2011-11-05 14:23:49 +0100

    fix proxy.php

--- web/proxy.php
+++ web/proxy.php
@@ -1,4 +1,8 @@
 <?php
+$allowedDomains = array(
+	'sfbg.us', 'www.sfbg.us',
+);
+
 if (isset($_GET['url'])) {
 	$url = urlencode($_GET['url']);
 	$url = strtr($url, array(
@@ -9,6 +13,12 @@ if (isset($_GET['url'])) {
 		'%26' => '&',
 	));
 
+	if (strpos($url, 'http://') !== 0) {
+		die('not a http address');
+	}
+	if ( ! in_array(parse_url($url, PHP_URL_HOST), $allowedDomains)) {
+		die('not allowed domain');
+	}
 	$response = @file_get_contents($url);
 	if ($response === false) {
 		echo 'error';
