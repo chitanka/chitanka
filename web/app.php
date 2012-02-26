45f47ef88c34d52d31033a3e156068c8c57bf847
borislav <b.manolov@gmail.com>
2012-02-26 10:20:05 +0100

    register_shutdown_function to catch fatal errors in production

--- web/app.php
+++ web/app.php
@@ -6,6 +6,15 @@ require_once __DIR__.'/../app/AppCache.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
+register_shutdown_function(function(){
+	$error = error_get_last();
+	if ($error['type'] == E_ERROR) {
+		ob_clean();
+		header('HTTP/1.1 503 Service Unavailable');
+		readfile(__DIR__ . '/503.html');
+	}
+});
+
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 $kernel = new AppCache($kernel);
