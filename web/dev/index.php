f996674d359dd759d7f4fd81ded9654649eade41
borislav <b.manolov@gmail.com>
2012-06-25 21:30:22 +0200

    mv: app.dev -> index.php; app_dev -> dev/index

--- web/dev/index.php
+++ web/dev/index.php
@@ -7,15 +7,16 @@
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it, or make something more sophisticated.
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
-    '127.0.0.1',
-    '::1',
+	'127.0.0.1',
+	'::1',
 ))) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+	header('HTTP/1.0 403 Forbidden');
+	exit('You are not allowed to access this file.');
 }
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
-require_once __DIR__.'/../app/AppKernel.php';
+chdir('..');
+require_once __DIR__.'/../../app/bootstrap.php.cache';
+require_once __DIR__.'/../../app/AppKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
