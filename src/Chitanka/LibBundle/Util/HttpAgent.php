f908f9a5a8c724bab550159ecece69b22e61d76b
borislav <b.manolov@gmail.com>
2012-08-26 09:38:29 +0200

    [command] new VerifyPersonInfoLinksDbCommand

---
+++ src/Chitanka/LibBundle/Util/HttpAgent.php
@@ -0,0 +1,30 @@
+<?php
+namespace Chitanka\LibBundle\Util;
+
+class HttpAgent
+{
+	public function urlExists($url)
+	{
+		$c = curl_init();
+		curl_setopt_array($c, array(
+			CURLOPT_URL             => $url,
+			CURLOPT_RETURNTRANSFER  => true,
+			CURLOPT_CUSTOMREQUEST   => 'HEAD',
+			CURLOPT_HEADER          => true,
+			// Wikimedia requires a proper user-agent string
+			// http://mediawiki.org/wiki/API:Quick_start_guide#Identifying_your_client
+			CURLOPT_USERAGENT       => 'User-Agent: PHP (chitanka.info link checker, http://chitanka.info/feedback)',
+		));
+		$res = curl_exec($c);
+		curl_close($c);
+
+		if (preg_match('/ERR_ACCESS_DENIED/', $res)) {
+			// an error page is served
+			// could not determine if the URL exists or not
+			return true;
+		}
+		$exists = preg_match('/^HTTP.+ 200 OK/', $res);
+
+		return $exists;
+	}
+}
