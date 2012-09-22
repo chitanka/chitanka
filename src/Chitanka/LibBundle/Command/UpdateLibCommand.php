743e2425278053ab9d0f932bdc0480ea584ad384
borislav <b.manolov@gmail.com>
2012-09-22 22:36:36 +0200

    rm unused “use” statement

--- src/Chitanka/LibBundle/Command/UpdateLibCommand.php
+++ src/Chitanka/LibBundle/Command/UpdateLibCommand.php
@@ -6,7 +6,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
-use Chitanka\LibBundle\Legacy\Setup;
 use Chitanka\LibBundle\Legacy\Legacy;
 use Chitanka\LibBundle\Util\String;
 use Chitanka\LibBundle\Util\File;
