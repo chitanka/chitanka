62228d1a87df6dacc0e6a762c76ef3a1b0a2234e
borislav <b.manolov@gmail.com>
2012-10-07 12:29:14 +0200

    [service] fix dir existance check

--- src/Chitanka/LibBundle/Service/DirectoryCopier.php
+++ src/Chitanka/LibBundle/Service/DirectoryCopier.php
@@ -12,7 +12,7 @@ class DirectoryCopier
 		foreach ($iterator as $item) {
 			$dest = $destDir . DIRECTORY_SEPARATOR . $iterator->getSubPathName();
 			if ($item->isDir()) {
-				if ( ! file_exists($item)) {
+				if ( ! file_exists($dest)) {
 					mkdir($dest);
 				}
 			} else {
