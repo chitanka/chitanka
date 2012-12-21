b01243140c2eafad3846cc6cff2af16c523bbff5
borislav <b.manolov@gmail.com>
2012-12-21 12:13:10 +0100

    [command] allow user to be skipped by workroom notification

--- src/Chitanka/LibBundle/Command/NotifyUsersForOldWorkEntriesCommand.php
+++ src/Chitanka/LibBundle/Command/NotifyUsersForOldWorkEntriesCommand.php
@@ -20,6 +20,7 @@ class NotifyUsersForOldWorkEntriesCommand extends CommonDbCommand
 			->setDescription('Notify all users with too old work entries')
 			->addArgument('age', InputArgument::REQUIRED, 'Threshold age for notification (in months)')
 			->addArgument('stalk-interval', InputArgument::OPTIONAL, 'Number of days between two subsequent notifications. Default: 7', 7)
+			->addArgument('skip-users', InputArgument::OPTIONAL, 'List of users by name which should not get notifications. Format: USERNAME1[,USERNAME2]*')
 			->setHelp(<<<EOT
 Notify all users with too old work entries.
 EOT
@@ -29,8 +30,12 @@ EOT
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
 		$oldEntries = $this->getRepository('WorkEntry')->findOlderThan($this->getThresholdDate($input));
+		$skippedUsers = $this->getSkippedUsers($input);
 		$notifer = new Notifier($this->getContainer()->get('mailer'));
 		foreach ($oldEntries as $entry) {
+			if ($this->shouldSkipEntry($entry, $skippedUsers)) {
+				continue;
+			}
 			$this->sendNotification($notifer, $entry, $input->getArgument('stalk-interval'));
 		}
 		$this->getEntityManager()->flush();
@@ -51,5 +56,15 @@ EOT
 	{
 		$age = $input->getArgument('age');
 		return date('Y-m-d', strtotime("-$age months"));
+	}
+
+	private function getSkippedUsers(InputInterface $input)
+	{
+		return explode(',', $input->getArgument('skip-users'));
+	}
+
+	private function shouldSkipEntry(WorkEntry $entry, array $skippedUsers)
+	{
+		return in_array($entry->getUser()->getUsername(), $skippedUsers);
 	}
 }
