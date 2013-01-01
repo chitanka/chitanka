f82cb8c7732ed50719b5f2153586a05d68111a3d
borislav <b.manolov@gmail.com>
2013-01-01 12:23:31 +0100

    put a simple cache for anonymous users

--- web/index.php
+++ web/index.php
@@ -1,4 +1,101 @@
 <?php
+function isCacheable() {
+	return $_SERVER['REQUEST_METHOD'] == 'GET' && !array_key_exists('mlt', $_COOKIE);
+}
+class Cache {
+	private $file;
+	private $request;
+	private $debug = false;
+
+	public function __construct($requestUri, $cacheDir) {
+		$hash = md5($requestUri);
+		$this->file = new CacheFile("$cacheDir/$hash[0]/$hash[1]/$hash[2]/$hash");
+		$this->request = $requestUri;
+	}
+
+	public function get() {
+		if ( ! $this->file->exists()) {
+			return null;
+		}
+		$ttl = $this->file->getRemainingTtl();
+		if ($ttl <= 0) {
+			$this->purge();
+			return null;
+		}
+		$this->log("=== CACHE HIT");
+		return array(
+			'data' => $this->file->read(),
+			'ttl' => $ttl,
+		);
+	}
+	public function set($content, $ttl) {
+		if ( ! $ttl) {
+			return;
+		}
+		$this->file->write($content);
+		$this->file->setTtl($ttl);
+		$this->log("+++ CACHE MISS ($ttl)");
+	}
+	private function purge() {
+		$this->file->delete();
+		$this->log('--- CACHE PURGE');
+	}
+	private function log($msg) {
+		if ($this->debug) {
+			error_log("$msg - $this->request");
+		}
+	}
+}
+class CacheFile {
+	private $name;
+
+	public function __construct($name) {
+		$this->name = $name;
+	}
+	public function exists() {
+		return file_exists($this->name);
+	}
+	public function write($content) {
+		$dir = dirname($this->name);
+		if ( ! file_exists($dir)) {
+			mkdir($dir, 0777, true);
+		}
+		file_put_contents($this->name, gzdeflate($content));
+	}
+	public function read() {
+		$content = file_get_contents($this->name);
+		if (empty($content) || $content[0] == '<') { // not compressed
+			return $content;
+		}
+		return gzinflate($content);
+	}
+	public function delete() {
+		unlink($this->name);
+		unlink("$this->name.ttl");
+	}
+	public function setTtl($value) {
+		file_put_contents("$this->name.ttl", $value);
+	}
+	public function getTtl() {
+		return file_get_contents("$this->name.ttl");
+	}
+	public function getRemainingTtl() {
+		$origTtl = $this->getTtl() + rand(0, 30) /* guard for race conditions */;
+		return $origTtl - $this->getAge();
+	}
+	public function getAge() {
+		return time() - filemtime($this->name);
+	}
+}
+
+$cache = new Cache($_SERVER['REQUEST_URI'], __DIR__.'/../app/cache/prod/simple_http_cache');
+
+if (isCacheable() && null !== ($cachedContent = $cache->get())) {
+	header("Cache-Control: public, max-age=".$cachedContent['ttl']);
+	echo $cachedContent['data'];
+	return;
+}
+
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -14,7 +111,7 @@ try {
 }
 
 require_once $rootDir.'/app/AppKernel.php';
-require_once $rootDir.'/app/AppCache.php';
+//require_once $rootDir.'/app/AppCache.php';
 
 register_shutdown_function(function(){
 	$error = error_get_last();
@@ -31,8 +128,14 @@ register_shutdown_function(function(){
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-$kernel = new AppCache($kernel);
+//$kernel = new AppCache($kernel);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
+if (isCacheable() && $response->isOk()) {
+	try {
+		$cache->set($response->getContent(), $response->getTtl());
+	} catch (\RuntimeException $e) {
+	}
+}
 $response->send();
 $kernel->terminate($request, $response);
