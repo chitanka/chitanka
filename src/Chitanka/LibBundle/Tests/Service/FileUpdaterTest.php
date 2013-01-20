b94b5110687cfcdb2e21116988a98d04d63a797a
borislav <b.manolov@gmail.com>
2013-01-20 19:19:09 +0100

    test stub for FileUpdater

---
+++ src/Chitanka/LibBundle/Tests/Service/FileUpdaterTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Chitanka\LibBundle\Tests\Service;
+
+use Chitanka\LibBundle\Tests\TestCase;
+use Chitanka\LibBundle\Service\FileUpdater;
+
+class FileUpdaterTest extends TestCase {
+
+	public function testRmdir()
+	{
+		$updater = new FileUpdater;
+	}
+}
