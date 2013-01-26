b0f50f7ea294eaf39ca91daf0574599d1ca6c063
borislav <b.manolov@gmail.com>
2013-01-26 15:52:35 +0100

    mv umask setting in front controllers

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -47,9 +47,6 @@ class AppKernel extends Kernel
 
 	public function registerContainerConfiguration(LoaderInterface $loader)
 	{
-		// allow generated files (cache, logs) to be world-writable
-		umask(0000);
-
 		$loader->load($this->getConfigurationFile($this->getEnvironment()));
 	}
 
