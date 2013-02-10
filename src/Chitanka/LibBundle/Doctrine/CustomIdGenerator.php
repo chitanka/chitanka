076af09cbac89c5adc0a7992f56177ee762e52e5
borislav <b.manolov@gmail.com>
2013-02-10 12:24:46 +0100

    [entity] refactor NextId generation, use raw SQL
    Use raw SQL, em->persist and em->flush causes an infinite loop in
    UnitOfWork::cascadePersist (line 2128: associationMappings = array_filter).

--- src/Chitanka/LibBundle/Doctrine/CustomIdGenerator.php
+++ src/Chitanka/LibBundle/Doctrine/CustomIdGenerator.php
@@ -3,41 +3,19 @@ namespace Chitanka\LibBundle\Doctrine;
 
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Id\AbstractIdGenerator;
-use Chitanka\LibBundle\Entity\NextId;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Chitanka\LibBundle\Entity\NextIdRepository;
 
 class CustomIdGenerator extends AbstractIdGenerator
 {
-
 	public function generate(EntityManager $em, $entity)
 	{
-		$nextId = $this->findNextId($em, $entity);
-		$entityId = $nextId->getValue();
-		$this->incrementAndSaveNextId($em, $nextId);
-
-		return $entityId;
+		return $this->createNextIdRepository($em)->selectNextId($entity);
 	}
 
-	/** @return NextId */
-	private function findNextId(EntityManager $em, $entity)
+	protected function createNextIdRepository(EntityManager $em)
 	{
-		$nextId = $em->find('LibBundle:NextId', get_class($entity));
-		if ($nextId == false) {
-			$nextId = new NextId(get_class($entity));
-			$nextId->setValue($this->getMaxIdForEntity($em, $entity) + 1);
-		}
-		return $nextId;
+		return new NextIdRepository($em, new ClassMetadata('Chitanka\LibBundle\Entity\NextId'));
 	}
 
-	private function incrementAndSaveNextId(EntityManager $em, NextId $nextId)
-	{
-		$nextId->increment();
-		$em->persist($nextId);
-		$em->flush();
-	}
-
-	private function getMaxIdForEntity(EntityManager $em, $entity)
-	{
-		$query = $em->createQuery(sprintf('SELECT MAX(e.id) FROM %s e', get_class($entity)));
-		return $query->getSingleScalarResult();
-	}
 }
