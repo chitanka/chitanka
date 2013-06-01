6c1eb6d809d9846fe8418060a120df1081a29d6a
borislav <b.manolov@gmail.com>
2013-06-01 18:00:03 +0200

    [js] gamebook enhancements - input fields; chance table

--- src/Chitanka/LibBundle/Resources/public/js/text.js
+++ src/Chitanka/LibBundle/Resources/public/js/text.js
@@ -43,7 +43,41 @@ function prepareGamebook()
 
 	$epHeader.hide();
 	driver.hideAll();
+
+	var enhanceChanceTable = function(idx, table) {
+		var $table = $(table);
+		$table.find("td").css({ padding: "1em 2em", "text-align": "center" });
+		var $cells = $table.find("td");
+		var $handle = $('<button style="display: block; margin: .5em auto">Случайно число</button>').click(function() {
+			var randIdx = Math.floor(Math.random() * $cells.length);
+			$cells.removeClass("hilite").eq(randIdx).addClass("hilite");
+			$(this).blur();
+		}).appendTo($table.find("caption")[0] || $('<caption/>').appendTo($table));
+	};
+	$(".js-chance-table table").each(enhanceChanceTable);
+
+	var enhanceInputCell = function(idx, cell) {
+		var $cell = $(cell);
+		var name = namePrefix + "-" + idx;
+		var htmlInput = $cell.text().replace(/…+/, '<input type="text" name="'+name+'" style="width: 100%">');
+		$cell.html(htmlInput);
+		$cell.on("change", ":input", function() {
+			localStorage[this.name] = this.value;
+		});
+		$cell.find(":input").each(function() {
+			if (localStorage[this.name]) {
+				$(this).val(localStorage[this.name]);
+			}
+		});
+	};
+	var $inputTables = $(".js-gamebook-input").find("table");
+	$inputTables.css({ width: "100%" });
+	var $inputCells = $inputTables.find("td");
+	var namePrefix = location.pathname;
+	$inputCells.each(enhanceInputCell);
+
 }
+
 
 // goto next chapter links
 $(document.body).on("click", "a[rel=next]", function(){
