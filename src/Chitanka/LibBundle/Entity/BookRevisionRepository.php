39e78ce19e272c4da9c3b1d2146abf99bb4afa36
borislav <b.manolov@gmail.com>
2013-10-06 10:16:31 +0200

    [entity] fix fetching of Authors relation by revision repos

--- src/Chitanka/LibBundle/Entity/BookRevisionRepository.php
+++ src/Chitanka/LibBundle/Entity/BookRevisionRepository.php
@@ -10,10 +10,11 @@ class BookRevisionRepository extends RevisionRepository
 	public function getQueryBuilder($orderBys = null)
 	{
 		$qb = parent::getQueryBuilder($orderBys)
-			->select('r', 'b', 'a', 's', 'c')
+			->select('r', 'b', 'a', 'ap', 's', 'c')
 			->from($this->getEntityName(), 'r')
 			->leftJoin('r.book', 'b')
-			->leftJoin('b.authors', 'a')
+			->leftJoin('b.bookAuthors', 'a')
+			->leftJoin('a.person', 'ap')
 			->leftJoin('b.sequence', 's')
 			->leftJoin('b.category', 'c');
 
--- src/Chitanka/LibBundle/Entity/TextRevisionRepository.php
+++ src/Chitanka/LibBundle/Entity/TextRevisionRepository.php
@@ -10,10 +10,11 @@ class TextRevisionRepository extends RevisionRepository
 	public function getQueryBuilder($orderBys = null)
 	{
 		$qb = parent::getQueryBuilder($orderBys)
-			->select('r', 't', 'a')
+			->select('r', 't', 'a', 'ap')
 			->from($this->getEntityName(), 'r')
 			->leftJoin('r.text', 't')
-			->leftJoin('t.authors', 'a');
+			->leftJoin('t.textAuthors', 'a')
+			->leftJoin('a.person', 'ap');
 
 		return $qb;
 	}
