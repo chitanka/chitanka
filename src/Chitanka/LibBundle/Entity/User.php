3454495f76adc9c2839d1921d91d69cc3186e273
borislav <b.manolov@gmail.com>
2013-12-23 17:51:56 +0100

    rm dev code

--- src/Chitanka/LibBundle/Entity/User.php
+++ src/Chitanka/LibBundle/Entity/User.php
@@ -639,7 +639,7 @@ class User /*extends BaseUser*/ implements UserInterface
 	{
 		return array(
 			'skin' => $this->option('skin', 'orange'),
-			'menu' => 'left',#$this->option('nav', 'right'),
+			'menu' => $this->option('nav', 'right'),
 		);
 	}
 }
