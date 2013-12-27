206762d16e111dc5b101ae1b9759167f9eef34bd
borislav <b.manolov@gmail.com>
2013-12-27 12:31:02 +0100

    [config] rm unused user css and js files

--- src/Chitanka/LibBundle/Resources/public/js/move_projects_links.js
+++
@@ -1,7 +0,0 @@
-user_functions.push(function(){
-	$("#project-links")
-		.removeClass("project-links")
-		.addClass("navbody")
-		.find("ul").addClass("textmenu").end()
-		.appendTo("#nav-main");
-});
