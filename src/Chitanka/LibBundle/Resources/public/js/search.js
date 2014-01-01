2fae6af70b68e9c579fe72a79b3a142964a35eef
borislav <b.manolov@gmail.com>
2014-01-01 19:03:36 +0100

    [js] remove not needed javascript files

--- src/Chitanka/LibBundle/Resources/public/js/search.js
+++
@@ -1,9 +0,0 @@
-$("input.search").each(function(){
-	var def = "Търсене на…";
-
-	$(this).bind("focus", function(){
-		this.value == def && $(this).removeClass("helpinput").val("");
-	}).bind("blur", function(){
-		this.value === "" && $(this).addClass("helpinput").val(def);
-	}).trigger("blur");
-});
