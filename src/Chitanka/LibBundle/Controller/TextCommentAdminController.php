29e98f06565a84049d2b5197dfe82f1203966ce8
borislav <b.manolov@gmail.com>
2014-01-02 09:29:42 +0100

    [admin] disable Csrf for comments

--- src/Chitanka/LibBundle/Controller/TextCommentAdminController.php
+++ src/Chitanka/LibBundle/Controller/TextCommentAdminController.php
@@ -4,5 +4,10 @@ namespace Chitanka\LibBundle\Controller;
 
 class TextCommentAdminController extends CRUDController
 {
+	public function validateCsrfToken($intention)
+	{
+		// disable for now
+		return true;
+	}
 
 }
