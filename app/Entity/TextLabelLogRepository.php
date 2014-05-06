929fba873a86967dede03cb914712f8da325fefa
borislav <b.manolov@gmail.com>
2014-05-06 07:22:55 +0200

    refactor TextLabelLogRepository

--- app/Entity/TextLabelLogRepository.php
+++ app/Entity/TextLabelLogRepository.php
@@ -6,27 +6,24 @@
 class TextLabelLogRepository extends EntityRepository {
 
 	public function getAll($page = 1, $limit = 30) {
-		$query = $this->createQueryBuilder('log')
-			->select('log', 'text', 'label', 'user')
-			->leftJoin('log.text', 'text')
-			->leftJoin('log.label', 'label')
-			->leftJoin('log.user', 'user')
-			->addOrderBy('log.date', 'desc')
-			->getQuery();
+		$query = $this->createDefaultQueryBuilder('log')->getQuery();
 		$this->setPagination($query, $page, $limit);
 		return $query->getArrayResult();
 	}
 
 	public function getForText(Text $text) {
-		$query = $this->createQueryBuilder('log')
-			->select('log', 'text', 'label', 'user')
-			->leftJoin('log.text', 'text')
-			->leftJoin('log.label', 'label')
-			->leftJoin('log.user', 'user')
+		$query = $this->createDefaultQueryBuilder('log')
 			->where('log.text = ?1')->setParameter(1, $text)
-			->addOrderBy('log.date', 'asc')
 			->getQuery();
 		return $query->getArrayResult();
 	}
 
+	private function createDefaultQueryBuilder($alias = 'log') {
+		return $this->createQueryBuilder($alias)
+			->select($alias, 'text', 'label', 'user')
+			->leftJoin("$alias.text", 'text')
+			->leftJoin("$alias.label", 'label')
+			->leftJoin("$alias.user", 'user')
+			->addOrderBy("$alias.date", 'desc');
+	}
 }
