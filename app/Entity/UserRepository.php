6b05a2e61e0bda8eeb5521163bac98b508147919
borislav <b.manolov@gmail.com>
2014-05-07 18:41:04 +0200

    add docblocks by UserRepository

--- app/Entity/UserRepository.php
+++ app/Entity/UserRepository.php
@@ -8,6 +8,10 @@ use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
  *
  */
 class UserRepository extends EntityRepository implements UserProviderInterface {
+
+	/**
+	 * @return User
+	 */
 	public function loadUserByUsername($username) {
 		$user = $this->findByUsername($username);
 		if ( ! $user) {
@@ -17,22 +21,37 @@ class UserRepository extends EntityRepository implements UserProviderInterface {
 		return $user;
 	}
 
+	/**
+	 * @return User
+	 */
 	public function findByUsername($username) {
 		return $this->findOneBy(array('username' => $username));
 	}
 
+	/**
+	 * @return User[]
+	 */
 	public function findByUsernames(array $usernames) {
 		return $this->findBy(array('username' => $usernames));
 	}
 
+	/**
+	 * @return User
+	 */
 	public function findByToken($token) {
 		return $this->findOneBy(array('token' => $token));
 	}
 
+	/**
+	 * @return User
+	 */
 	public function refreshUser(UserInterface $user) {
 		return $user;
 	}
 
+	/**
+	 * @return bool
+	 */
 	public function supportsClass($class) {
 		return false;
 	}
