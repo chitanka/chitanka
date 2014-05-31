5e748fd88f46369b0970af9acd0ee6453311a2ff
borislav <b.manolov@gmail.com>
2014-05-31 18:26:59 +0200

    set response maxage - allow private caching by browser

--- app/Controller/Controller.php
+++ app/Controller/Controller.php
@@ -181,6 +181,7 @@ abstract class Controller extends SymfonyController {
 	protected function setCacheStatusByResponse(Response $response) {
 		if ($this->responseAge && $this->container->getParameter('use_http_cache')) {
 			$response->setSharedMaxAge($this->responseAge);
+			$response->setMaxAge($this->responseAge);
 		}
 		return $response;
 	}
