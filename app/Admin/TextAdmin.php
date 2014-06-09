db260db4a3cb8fe9fcabf15427dc10d275c6be83
borislav <b.manolov@gmail.com>
2014-06-09 22:27:09 +0200

    rm unnecessary use statement

--- app/Admin/TextAdmin.php
+++ app/Admin/TextAdmin.php
@@ -7,7 +7,6 @@ use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
-use Sonata\AdminBundle\Route\RouteCollection;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormEvent;
 
