db96b636ee66d23f54591f8fb0964853e9a2d68d
borislav <b.manolov@gmail.com>
2015-03-28 14:11:04 +0100

    refactor search controller - move code to the search service

--- app/Controller/SearchController.php
+++ app/Controller/SearchController.php
@@ -1,12 +1,9 @@
 <?php namespace App\Controller;
 
-use App\Entity\SearchString;
 use App\Service\SearchService;
 use Symfony\Component\HttpFoundation\Request;
 
 class SearchController extends Controller {
-
-	const MAX_RESULTS = 50;
 
 	public function indexAction(Request $request, $_format) {
 		if ($_format == 'osd') {
@@ -18,40 +15,13 @@ class SearchController extends Controller {
 			return $query;
 		}
 
-		$lists = [
-			'persons'      => $this->em()->getPersonRepository()->getByNames($query['text'], self::MAX_RESULTS),
-			'texts'        => $this->em()->getTextRepository()->getByTitles($query['text'], self::MAX_RESULTS),
-			'books'        => $this->em()->getBookRepository()->getByTitleOrIsbn($query['text'], self::MAX_RESULTS),
-			'series'       => $this->em()->getSeriesRepository()->getByNames($query['text'], self::MAX_RESULTS),
-			'sequences'    => $this->em()->getSequenceRepository()->getByNames($query['text'], self::MAX_RESULTS),
-			'work_entries' => $this->em()->getWorkEntryRepository()->getByTitleOrAuthor($query['text']),
-			'labels'       => $this->em()->getLabelRepository()->getByNames($query['text']),
-			'categories'   => $this->em()->getCategoryRepository()->getByNames($query['text']),
-		];
-
-		$found = array_sum(array_map('count', $lists)) > 0;
-
-		if ($found) {
-			$this->logSearch($query['text']);
-		}
+		$result = $searchService->executeSearch($query);
 
 		return [
 			'query' => $query,
-			'found' => $found,
-			'_status' => !$found ? 404 : null,
-		] + $lists;
-	}
-
-	/**
-	 * @param string $query
-	 */
-	private function logSearch($query) {
-		$searchString = $this->em()->getSearchStringRepository()->findByName($query);
-		if ( ! $searchString) {
-			$searchString = new SearchString($query);
-		}
-		$searchString->incCount();
-		$this->em()->getSearchStringRepository()->save($searchString);
+			'result' => $result,
+			'_status' => $result->isEmpty() ? 404 : null,
+		];
 	}
 
 }
--- app/Service/SearchService.php
+++ app/Service/SearchService.php
@@ -1,10 +1,17 @@
 <?php namespace App\Service;
 
 use App\Entity\EntityManager;
+use App\Entity\SearchString;
 use App\Util\String;
 use Symfony\Component\HttpFoundation\Request;
 
 class SearchService {
+
+	const MAX_RESULTS = 50;
+	const MAX_LATEST_STRINGS = 30;
+	const MAX_TOP_STRINGS = 30;
+
+	const HTTP_STATUS_INVALID = 400;
 
 	private static $minQueryLength = 3;
 	private static $maxQueryLength = 60;
@@ -22,8 +29,8 @@ class SearchService {
 		if (empty($query)) {
 			return [
 				'_template' => "App:Search:list_top_strings.$format.twig",
-				'latest_strings' => $this->em->getSearchStringRepository()->getLatest(30),
-				'top_strings' => $this->em->getSearchStringRepository()->getTop(30),
+				'latest_strings' => $this->em->getSearchStringRepository()->getLatest(self::MAX_LATEST_STRINGS),
+				'top_strings' => $this->em->getSearchStringRepository()->getTop(self::MAX_TOP_STRINGS),
 			];
 		}
 
@@ -36,7 +43,7 @@ class SearchService {
 			} catch (\InvalidArgumentException $e) {
 				return [
 					'_template' => "App:Search:message.$format.twig",
-					'_status' => 400,
+					'_status' => self::HTTP_STATUS_INVALID,
 					'message' => $e->getMessage(),
 				];
 			}
@@ -50,6 +57,27 @@ class SearchService {
 	}
 
 	/**
+	 * @param array $query
+	 * @return SearchResult
+	 */
+	public function executeSearch(array $query) {
+		$result = new SearchResult([
+			'persons'      => $this->em->getPersonRepository()->getByNames($query['text'], self::MAX_RESULTS),
+			'texts'        => $this->em->getTextRepository()->getByTitles($query['text'], self::MAX_RESULTS),
+			'books'        => $this->em->getBookRepository()->getByTitleOrIsbn($query['text'], self::MAX_RESULTS),
+			'series'       => $this->em->getSeriesRepository()->getByNames($query['text'], self::MAX_RESULTS),
+			'sequences'    => $this->em->getSequenceRepository()->getByNames($query['text'], self::MAX_RESULTS),
+			'work_entries' => $this->em->getWorkEntryRepository()->getByTitleOrAuthor($query['text']),
+			'labels'       => $this->em->getLabelRepository()->getByNames($query['text']),
+			'categories'   => $this->em->getCategoryRepository()->getByNames($query['text']),
+		]);
+		if (!$result->isEmpty()) {
+			$this->logSearch($query['text']);
+		}
+		return $result;
+	}
+
+	/**
 	 * @param string $query
 	 */
 	private function validateQueryLength($query) {
@@ -60,6 +88,34 @@ class SearchService {
 		if ($queryLength > self::$maxQueryLength) {
 			throw new \InvalidArgumentException(sprintf('Не може да въвеждате повече от %d знака.', self::$maxQueryLength));
 		}
+	}
+
+	/**
+	 * @param string $query
+	 */
+	private function logSearch($query) {
+		$searchString = $this->em->getSearchStringRepository()->findByName($query);
+		if ( ! $searchString) {
+			$searchString = new SearchString($query);
+		}
+		$searchString->incCount();
+		$this->em->getSearchStringRepository()->save($searchString);
+	}
+}
+
+class SearchResult {
+
+	public $nbResults;
+
+	public function __construct(array $lists) {
+		$this->nbResults = array_sum(array_map('count', $lists)) > 0;
+		foreach ($lists as $entity => $list) {
+			$this->$entity = $list;
+		}
+	}
+
+	public function isEmpty() {
+		return $this->nbResults == 0;
 	}
 
 }
