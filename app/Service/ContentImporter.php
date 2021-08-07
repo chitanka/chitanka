af415dc10993dc68dcc93798ccd5389e8fafb133
borislav <b.manolov@gmail.com>
2021-08-07 15:31:38 +0200

    Allow alphanumeric IDs in source book templates
    Example of a source template file for a book:
    
        {text:-1}
        {text:-2}
        {text:-3}
    
    If for some reason we want to insert a text between 2 and 3, we must regenerate all existing files so that we can insert an integer number inbetween.
    This will give us following:
    
        {text:-1}
        {text:-2}
        {text:-3}
        {text:-4}
    
    As this has no effect on the generated real IDs, it would make sense to allow some other schema for such additional IDs.
    For this reason it is now possible to use alphanumeric IDs when such a need arises.
    Example:
    
        {text:-1}
        {text:-2}
        {text:-2a}
        {text:-3}

--- app/Service/ContentImporter.php
+++ app/Service/ContentImporter.php
@@ -271,7 +271,7 @@ class ContentImporter {
 	private static function sortDataFiles($files) {
 		$sorted = [];
 		foreach ($files as $file) {
-			if (preg_match('/\.(\d+)\.data/', $file, $matches) && filesize($file)) {
+			if (preg_match('/\.(\d+\w?)\.data/', $file, $matches) && filesize($file)) {
 				$sorted[$matches[1]] = $file;
 			}
 		}
@@ -283,7 +283,7 @@ class ContentImporter {
 	private static function getBookTemplate($file, $works) {
 		$bookTmpl = file_get_contents($file);
 		$bookWorks = [];
-		if (preg_match_all('/\{(file|text):(\d+)/', $bookTmpl, $m)) {
+		if (preg_match_all('/\{(file|text):(\d+\w?)/', $bookTmpl, $m)) {
 			// already existing in the database works included in this book
 			foreach ($m[2] as $oldWork) {
 				$bookWorks[] = ['id' => $oldWork, 'is_new' => false];
@@ -306,10 +306,10 @@ class ContentImporter {
 	private static function prepareWorkTemplate($work) {
 		$files = [];
 		$template = file_get_contents($work['tmpl']);
-		if (preg_match_all('/\{(text|file):-\d+(-.+)\}/', $template, $matches)) {
+		if (preg_match_all('/\{(text|file):-[\d\w]+(-.+)\}/', $template, $matches)) {
 			foreach ($matches[2] as $match) {
 				$files["$work[id]$match"] = str_replace('.tmpl', "$match.text", $work['tmpl']);
-				$template = preg_replace("/(text|file):-\d+-/", "$1:$work[id]-", $template);
+				$template = preg_replace("/(text|file):-[\d\w]+-/", "$1:$work[id]-", $template);
 			}
 		}
 		$work['text'] = $files;
