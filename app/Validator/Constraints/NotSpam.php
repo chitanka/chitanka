ac0902e64c3b4ecddfb076f604a02e8c54edadd7
borislav <b.manolov@gmail.com>
2022-10-12 17:06:59 +0200

    Move the spam phrases into a separate file

--- app/Validator/Constraints/NotSpam.php
+++ app/Validator/Constraints/NotSpam.php
@@ -8,9 +8,18 @@ use Symfony\Component\Validator\Constraint;
 class NotSpam extends Constraint {
 	public $message = 'notspam';
 	public $urlLimit = 2;
-	public $stopWords = [
-		'CASH & CARRY',
-		'НЕ ОТВЕЧАЙТЕ НА ЭТО ПИСЬМО',
-		' еба ',
-	];
+	public $stopWords = [];
+	private $stopWordsFile = __DIR__.'/../../config/spam_phrases.ini';
+
+	public function __construct($options = null) {
+		parent::__construct($options);
+		$this->stopWords = $this->fetchStopWords();
+	}
+
+	private function fetchStopWords() {
+		$lines = file($this->stopWordsFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+		return array_filter($lines, function(string $line) {
+			return $line[0] !== '#';
+		});
+	}
 }
