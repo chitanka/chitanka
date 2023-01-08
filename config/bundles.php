0bf03f4f7118768dc432e9600f5c072596afad0f
borislav <b.manolov@gmail.com>
2023-01-08 17:04:26 +0100

    [SF5 upgrade] Remove buzz package
    sensio/buzz-bundle is not maintained anymore and does work with Symfony 5
    
    The buzz library has also changed and is not compatible with current usage.
    
    TODO: Use some other browser package.

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Eko\FeedBundle\EkoFeedBundle::class => ['all' => true],
-    Sensio\Bundle\BuzzBundle\SensioBuzzBundle::class => ['all' => true],
 ];
